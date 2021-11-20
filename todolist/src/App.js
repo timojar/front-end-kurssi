6c1cefcc87c48958b22086b09281205b00abdedf
timojar <jarmalatimo@gmail.com>
2021-11-20 22:55:40 +0200

    Added dolist-component

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import './App.css'
+import ToDoTable from './TodoList'
 
 function App () {
-  const [todo, setTodo] = useState({desc: '', date: ''})
+  const [todo, setTodo] = useState({ desc: '', date: '' })
   const [todos, setTodos] = useState([])
 
-  const inputChanged = (event) => { setTodo({...todo, [event.target.name]: [event.target.value]}) }
+  const inputChanged = (event) => { setTodo({ ...todo, [event.target.name]: [event.target.value] }) }
   const addTodo = (event) => {
     event.preventDefault()
     setTodos([...todos, todo])
@@ -15,12 +16,9 @@ function App () {
     <div className="App">
 
         <input type="text" name="desc" onChange={inputChanged} value={todo.desc}/>
-        <input type="date" name="date" onChange={inputChanged} value={todo.date}/> 
+        <input type="date" name="date" onChange={inputChanged} value={todo.date}/>
         <button onClick={addTodo}> Add </button>
-
-        <table><tbody>{todos.map((todo, index) => <tr key={index}><td>{todo.desc}</td><td>{todo.date}</td></tr>)}
-              </tbody>
-        </table>
+        <ToDoTable todos={todos}/>  
 
     </div>
   )
