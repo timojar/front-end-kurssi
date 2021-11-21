127cba09fc68122927b9ef3a21830274620c4628
timojar <jarmalatimo@gmail.com>
2021-11-21 15:35:15 +0200

    delete button added

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,24 +1,39 @@
 import React, { useState } from 'react'
 import './App.css'
-import ToDoTable from './TodoList'
 
 function App () {
-  const [todo, setTodo] = useState({ desc: '', date: '' })
+  const [todo, setTodo] = useState({ desc: '', date: '', id: ''})
   const [todos, setTodos] = useState([])
 
-  const inputChanged = (event) => { setTodo({ ...todo, [event.target.name]: [event.target.value] }) }
+  const inputChanged = (event) => { 
+    setTodo({ ...todo, [event.target.name]: [event.target.value]}) 
+}
   const addTodo = (event) => {
     event.preventDefault()
-    setTodos([...todos, todo])
+   let editedToDo = todo
+   editedToDo.id = new Date().getTime()
+    setTodos([...todos, editedToDo])
+    setTodo({ desc: '', date: '', id: ''})
   }
+
+const deleteTodo =(id) =>{
+  let newTodos= todos.filter(item => item.id !== id); 
+  setTodos(newTodos)
+};
+
 
   return (
     <div className="App">
 
-        <input type="text" name="desc" onChange={inputChanged} value={todo.desc}/>
-        <input type="date" name="date" onChange={inputChanged} value={todo.date}/>
-        <button onClick={addTodo}> Add </button>
-        <ToDoTable todos={todos}/>  
+        <input type="date" name="date"onChange={inputChanged} value={todo.date}/>
+        <input type="text" name="desc" onChange={inputChanged} value={todo.desc}/> <button onClick={addTodo}> Add </button>
+
+        <table><tbody>{todos.map((todo, index) =><tr key={index}><td>{todo.desc} </td><td>{todo.date}</td>
+                                                        <button onClick={() => deleteTodo(todo.id)}> Delete </button>                                                     
+                                                </tr> )
+                      }
+              </tbody>
+        </table>
 
     </div>
   )
