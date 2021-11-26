bba557a9dcc7de94ee0b44f85b273d934d9f7e3b
timojar <jarmalatimo@gmail.com>
2021-11-26 19:28:11 +0200

    fixing code

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,36 +1,44 @@
-import React, { useState } from 'react'
-import './App.css'
-import ToDoTable from './TodoList'
+import React, { useState } from "react";
+import "./App.css";
+import ToDoTable from "./TodoList";
 
-function App () {
-  const [todo, setTodo] = useState({ desc: '', date: ''})
-  const [todos, setTodos] = useState([])
+function App() {
+  const [todo, setTodo] = useState({ desc: "", date: "" });
+  const [todos, setTodos] = useState([]);
 
-  const inputChanged = (event) => { 
-    setTodo({ ...todo, [event.target.name]: [event.target.value]}) 
-}
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: [event.target.value] });
+  };
   const addTodo = (event) => {
-    event.preventDefault();    
-    todo.id = new Date().getTime()
-    setTodos([...todos, todo])
-    setTodo({ desc: '', date: ''})
-  }
+    event.preventDefault();
+    todo.id = new Date().getTime();
+    setTodos([...todos, todo]);
+    setTodo({ desc: "", date: "" });
+  };
 
-const deleteTodo =(id) =>{
-  let newTodos= todos.filter(item => item.id !== id); 
-  setTodos(newTodos)
-};
-
+  const deleteTodo = (id) => {
+    let newTodos = todos.filter((item) => item.id !== id);
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
-
-        <input type="date" name="date"onChange={inputChanged} value={todo.date}/>
-        <input type="text" name="desc" onChange={inputChanged} value={todo.desc}/> <button onClick={addTodo}> Add </button>
-        <ToDoTable deleteTodo={deleteTodo} todos={todos}/>  
-
+      <input
+        type="date"
+        name="date"
+        onChange={inputChanged}
+        value={todo.date}
+      />
+      <input
+        type="text"
+        name="desc"
+        onChange={inputChanged}
+        value={todo.desc}
+      />{" "}
+      <button onClick={addTodo}> Add </button>
+      <ToDoTable deleteTodo={deleteTodo} todos={todos} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,18 +1,24 @@
 import React from "react";
 
-export default function ToDoList(props){
-
-    return (
-        <div>
-            
-            <table><tbody>{props.todos.map((todo, index) =><tr key={index}><td>{todo.date}</td><td>{todo.desc} </td>
-                                                        <button onClick={() => props.deleteTodo(todo.id)}> Delete </button>                                                     
-                                                </tr> )
-                      }
-              </tbody>
-        </table>
-        </div>
-
-    );
-
+export default function ToDoList(props) {
+  return (
+    <div>
+      
+      <table>
+        <tbody>
+          {props.todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo.date}</td>
+              <td>{todo.desc}</td>
+              <td>
+                <button onClick={() => props.deleteTodo(todo.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
