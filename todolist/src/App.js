fc2ec372c0ceba4c1eaa15a1e5e2d3b3b1a33ec6
timojar <jarmalatimo@gmail.com>
2021-11-27 18:18:44 +0200

    React Router implemented

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,84 +1,23 @@
 import React, { useState } from "react";
 import "./App.css";
-import { AgGridReact } from "ag-grid-react";
-import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-material.css";
+import Home from './Home'
+import Contact from './Contact'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const [todo, setTodo] = useState({ desc: "", date: "", priority: "" });
-  const [todos, setTodos] = useState([]);
-  const gridRef = React.useRef();
-
-  const inputChanged = (event) => {
-    setTodo({ ...todo, [event.target.name]: [event.target.value] });
-  };
-  const addTodo = (event) => {
-    event.preventDefault();
-    todo.id = new Date().getTime();
-    setTodos([...todos, todo]);
-  };
-
-  const deleteTodo = () => {
-    if (gridRef.current.getSelectedNodes().length > 0) {
-      setTodos(
-        todos.filter(
-          (todo, index) =>
-            index !== gridRef.current.getSelectedNodes()[0].childIndex
-        )
-      );
-    } else {
-      alert("Select row first");
-    }
-  };
-
-  const columns = [
-    { headerName: "Date", field: "date", sortable: true, filter: true },
-    { headerName: "Description", field: "desc", sortable: true, filter: true },
-    {
-      headerName: "Priority",
-      field: "priority",
-      sortable: true,
-      filter: true,
-      cellStyle: (params) =>
-        params.value === "High" ? { color: "red" } : { color: "black" },
-    },
-  ];
-
   return (
-    <div className="App">
-      <input
-        type="date"
-        name="date"
-        onChange={inputChanged}
-        value={todo.date}
-      />
-      <input
-        type="text"
-        name="desc"
-        onChange={inputChanged}
-        value={todo.desc}
-      />
-      <input
-        type="text"
-        name="priority"
-        onChange={inputChanged}
-        value={todo.priority}
-      />
-      <button onClick={addTodo}> Add </button>
-      <button onClick={deleteTodo}>Delete</button>
-      <div
-        className="ag-theme-material"
-        style={{ height: "700px", width: "70%", margin: "auto" }}
-      >
-        <AgGridReact
-          onGridReady={(params) => (gridRef.current = params.api)}
-          ref={gridRef}
-          rowSelection="single"
-          columnDefs={columns}
-          rowData={todos}
-        />
+    <Router>
+      <div>
+        <Link to="/"> Home </Link> <Link to="/about">About</Link>{" "}
+        <Link to="/contact">Contact</Link>{" "}       
+          <Routes>
+            <Route exact path="/"  element={< Home />}/>
+            <Route path="/about" render={() => <h3> About us</h3>} />
+            <Route path="/contact" element={< Contact />} />
+            <Route path="*" render={() =>  <h1> Not found</h1>} />
+          </Routes>        
       </div>
-    </div>
+    </Router>
   );
 }
 
