a161024096915f3707cf4f2a05ce2e09e3d61e03
timojar <jarmalatimo@gmail.com>
2021-11-28 17:45:12 +0200

    added all the components

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import "./App.css";
 import Home from './Home'
 import Contact from './Contact'
+import About from './About'
+import NotFound from './NotFound'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div>
+      <div class="App-header">
         <Link to="/"> Home </Link> <Link to="/about">About</Link>{" "}
         <Link to="/contact">Contact</Link>{" "}       
           <Routes>
             <Route exact path="/"  element={< Home />}/>
-            <Route path="/about" render={() => <h3> About us</h3>} />
+            <Route path="/about" element={< About />}/>
             <Route path="/contact" element={< Contact />} />
-            <Route path="*" render={() =>  <h1> Not found</h1>} />
+            <Route path="*" element={< NotFound />} />
           </Routes>        
       </div>
     </Router>
